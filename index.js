b0173cb4571ff164949d1f36d50c89a2f9064bf5
chenqinfeng <androidchenqinfeng@gmail.com>
2025-09-14 16:20:50 +0800

    fix issue of session secure

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24小时
   }
